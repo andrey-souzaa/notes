85f1d5e61e220d6f8369b98fafefa504dfb16836
Andrey Souza <carlos.andrey1005@gmail.com>
2025-10-02 17:13:17 -0300

    buda

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello World";
-});
-
-Route::get('/about', function(){
-    echo 'about us';
-});
-
-Route::get('/main',[MainController::class, 'index']);
+// auth route
+Route::get('/login',[AuthController::class, 'login']);
+Route::get('/loginSubmit',[AuthController::class, 'loginSubmit']);
+Route::get('/logout',[AuthController::class, 'logout']);
